a96d207b48cd523ca88cf29719fa4cbba1addc8b
DarlingtonEbom <darlingtonebom@gamil.com>
2023-05-24 01:15:55 -0700

    updated

--- shell.h
+++ shell.h
@@ -23,12 +23,6 @@
 /* Global environemnt */
 extern char **environ;
 
-/* Global program name */
-char *name;
-
-/* Global history counter */
-int hist;
-
 /**
  * struct list_s - A new struct type defining a linked list.
  * @dir: A directory path.
