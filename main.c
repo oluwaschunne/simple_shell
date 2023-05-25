4bba4ae2e88a255a9257d03a0507e2566535c503
oluwaschunne <falope.oluwaseun2@gmail.com>
2023-05-25 01:18:47 -0700

    Updated

--- main.c
+++ main.c
@@ -6,26 +6,29 @@
  *	Darlington Ebom
  */
 
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char **argv)
 {
 	const int ERROR_ACCESS = 126;
 	const int ERROR_NOENT = 127;
 	info_t info_list[] = { INFO_INIT };
 	int file_descriptor = 2;
-	
 	/* Modify the file descriptor by adding 3 to its value */
 	file_descriptor += 3;
-	
 	if (argc == 2)
 	{
 		int fd = open(argv[1], O_RDONLY);
-		
+
 		if (fd == -1)
 		{
-			
 			if (errno == EACCES)
 				exit(ERROR_ACCESS);
-			
 			if (errno == ENOENT)
 			{
 				eputs(argv[0]);
@@ -35,17 +38,12 @@ int main(int argc, char **argv)
 				_eputchar(BUF_FLUSH);
 				exit(ERROR_NOENT);
 			}
-			
 			return (EXIT_FAILURE);
 		}
-		
 		info_list->readfd = fd;
 	}
-	
 	populate_env_list(info_list);
 	read_history(info_list);
 	hsh(info_list, argv);
-	
 	return (EXIT_SUCCESS);
 }
-
