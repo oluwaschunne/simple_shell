3345637978195f8d632fd557c9ad0b9a39f1b4d7
DarlingtonEbom <darlingtonebom@gamil.com>
2023-05-25 02:20:45 -0700

    updated

--- get_env.c
+++ get_env.c
@@ -6,6 +6,12 @@
  *      Darlington Ebom
  */
 
+/**
+ * get_environ-returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
 char **get_environ(info_t *info);
 int unset_env(info_t *info, char *var);
 int set_env(info_t *info, char *var, char *value);
@@ -17,79 +23,95 @@ char **get_environ(info_t *info)
 		info->environ = convert_env_list(info->env);
 		info->env_changed = 0;
 	}
-	
+
 	return (info->environ);
 }
 
+/**
+ * unset_env-Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: 1 on delete, 0 otherwise
+ * @var:the string env var property
+ */
 int unset_env(info_t *info, char *var)
 {
 	list_t *node = info->env;
 	size_t index = 0;
 	char *p;
-	
+
 	if (!node || !var)
-		
+
 		return (0);
-	
+
 	while (node)
 	{
 		p = find_var_match(node->str, var);
-		
+
 		if (p && *p == '=')
 		{
 			info->env_changed = delete_node_at_index(&(info->env), index);
 			index = 0;
 			node = info->env;
-			
+
 			continue;
 		}
-		
+
 		node = node->next;
 		index++;
 	}
-	
+
 	return (info->env_changed);
 }
 
+/**
+ * set_env-initialize a new environment variable,
+ * or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * @var:the string env var property
+ * @value:the string env var value
+ * Return: Always 0
+ */
 int set_env(info_t *info, char *var, char *value)
 {
 	char *buf = NULL;
 	list_t *node;
 	char *p;
-	
+
 	if (!var || !value)
 		return (0);
-	
+
 	buf = malloc(strlen(var) + strlen(value) + 2);
-	
+
 	if (!buf)
 		return (1);
-	
+
 	strcpy(buf, var);
 	strcat(buf, "=");
 	strcat(buf, value);
-       
+
 	node = info->env;
 
 	while (node)
 	{
 		p = find_var_match(node->str, var);
-		
+
 		if (p && *p == '=')
 		{
 			free(node->str);
 			node->str = buf;
 			info->env_changed = 1;
-			
+
 			return (0);
 		}
-		
+
 		node = node->next;
 	}
-	
+
 	add_node_end(&(info->env), buf, 0);
 	free(buf);
 	info->env_changed = 1
-		
+
 		return (0);
 }
--- main.c
+++ main.c
@@ -12,20 +12,20 @@ int main(int argc, char **argv)
 	const int ERROR_NOENT = 127;
 	info_t info_list[] = { INFO_INIT };
 	int file_descriptor = 2;
-	
+
 	/* Modify the file descriptor by adding 3 to its value */
 	file_descriptor += 3;
-	
+
 	if (argc == 2)
 	{
 		int fd = open(argv[1], O_RDONLY);
-		
+
 		if (fd == -1)
 		{
-			
+
 			if (errno == EACCES)
 				exit(ERROR_ACCESS);
-			
+
 			if (errno == ENOENT)
 			{
 				eputs(argv[0]);
@@ -35,17 +35,17 @@ int main(int argc, char **argv)
 				_eputchar(BUF_FLUSH);
 				exit(ERROR_NOENT);
 			}
-			
+
 			return (EXIT_FAILURE);
 		}
-		
+
 		info_list->readfd = fd;
 	}
-	
+
 	populate_env_list(info_list);
 	read_history(info_list);
 	hsh(info_list, argv);
-	
+
 	return (EXIT_SUCCESS);
 }
 
